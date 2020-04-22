5f9f5f7193f87aa5fdb9b91fe24e5e2de9868e9d
TheGr8Chibi <43129699+TheGr8Chibi@users.noreply.github.com>
2020-04-22 14:38:38 +0200

    Fixed server crashes when closing clients
    TODO: make clients close when server disconnects
    maybe make username selection possible, or assign random names to users (zebra, tiger etc)

--- src/main/Server.java
+++ src/main/Server.java
@@ -3,6 +3,7 @@ package main;
 import java.net.*;
 import java.util.Vector;
 import java.io.*;
+import java.util.ArrayList; 
 
 public class Server {
 
@@ -16,9 +17,13 @@ public class Server {
 	static DataOutputStream output;
 
 	final static int port = 5006;
-	static Vector<read> ar = new Vector<>();
-	static int clientCount = 1;
-
+	
+	//Vector with clients
+	static Vector<client> clients = new Vector<>();
+	
+	//List for thread ID's
+	static ArrayList<Long> TS = new ArrayList<Long>();
+	
 	public static void main(String[] args) throws IOException {
 
 		server = new ServerSocket(port);
@@ -26,6 +31,8 @@ public class Server {
 		connector c = new connector();
 		Thread srv = new Thread(c);
 		srv.start();
+		System.out.println("Server started");
+		System.out.println();
 	}
 
 	static public class connector implements Runnable {
@@ -33,34 +40,36 @@ public class Server {
 			while (true) {
 				try {
 					socket = server.accept();
-					System.out.println("Client connected");
-
+					System.out.println("Client " + clients.size() + " connected");
+					System.out.println();
+					
 					input = new DataInputStream(socket.getInputStream());
 					output = new DataOutputStream(socket.getOutputStream());
 
-					read s = new read(socket, input, output, clientCount);
-					ar.add(s);
+					client s = new client(socket, input, output, clients.size());
+					clients.add(s);
 					t = new Thread(s);
 					t.start();
-					clientCount++;
+					TS.add(t.getId());
+					
 				} catch (IOException e) {
-					//e.printStackTrace();
+					// e.printStackTrace();
 				}
 			}
 		}
 	}
 
-	static public class read implements Runnable {
+	static public class client implements Runnable {
 		DataInputStream in;
 		DataOutputStream out;
 		Socket s;
 		int id;
 
-		public read(Socket socket, DataInputStream inR, DataOutputStream outR, int id) {
+		public client(Socket socket, DataInputStream inR, DataOutputStream outR, int idR) {
 			in = inR;
 			out = outR;
 			s = socket;
-			this.id = id;
+			id = idR;
 		}
 
 		@Override
@@ -70,21 +79,53 @@ public class Server {
 				try {
 					String inr = in.readUTF();
 					// Send message to all clients
-					for (read r : Server.ar) {
+					inr = "Client " + id + ": " + inr;
+					for (client r : Server.clients) {
 						if (r.id != id) {
 							try {
 								r.out.writeUTF(inr);
 							} catch (IOException e) {
-								//e.printStackTrace();
+								e.printStackTrace();
+								break;
 							}
 						}
 					}
 					System.out.println(inr);
 				} catch (IOException e) {
-					//e.printStackTrace();
+					// e.printStackTrace();
+					removeClient(s, in, out, id);
+					break;
 				}
 			}
 
+		}
+	}
+
+	public static void removeClient(Socket s, DataInputStream in, DataOutputStream out, int id) {
+		for (Thread t : Thread.getAllStackTraces().keySet()) {
+			if (t.getId() == TS.get(id) && TS.size() != 0) {
+				t.interrupt();
+				TS.remove(id);
+				try {
+					s.close();
+					in.close();
+					out.close();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				clients.remove(id);
+
+				for (client r : Server.clients) {
+					if (r.id > id && r.id != 0) {
+						r.id--;
+					}
+				}
+				System.out.println();
+				System.out.println("Client " + id + " disconnected");
+				System.out.println("Clients connected: " + clients.size());
+				System.out.println();
+				break;
+			}
 		}
 	}
 
